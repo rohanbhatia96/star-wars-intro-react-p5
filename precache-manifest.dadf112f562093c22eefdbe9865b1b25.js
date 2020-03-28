0e71593fce15ba2f791dc52d430a8cb00376963d
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-03-28 18:26:56 +0530

    Updates

---
+++ precache-manifest.dadf112f562093c22eefdbe9865b1b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601ed66194fd2ce745bf2f73aa05e108",
+    "url": "/star-wars-intro-react-p5/index.html"
+  },
+  {
+    "revision": "173ad1ab27bbf33d2a49",
+    "url": "/star-wars-intro-react-p5/static/css/main.cd1b03fe.chunk.css"
+  },
+  {
+    "revision": "3611b4d565b68ba66b8a",
+    "url": "/star-wars-intro-react-p5/static/js/2.174a42c4.chunk.js"
+  },
+  {
+    "revision": "4337af6fb824c0f74bd01d46f36be546",
+    "url": "/star-wars-intro-react-p5/static/js/2.174a42c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173ad1ab27bbf33d2a49",
+    "url": "/star-wars-intro-react-p5/static/js/main.e8131b70.chunk.js"
+  },
+  {
+    "revision": "8b4411a246d5a3ba5802",
+    "url": "/star-wars-intro-react-p5/static/js/runtime-main.810138de.js"
+  }
+]);
